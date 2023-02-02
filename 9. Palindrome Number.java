666b6e9f5dd1330bb025529711b7efb6b9f08dd5
shrutidoneria123 <95027763+shrutidoneria123@users.noreply.github.com>
2023-02-02 14:56:57 +0530

    Create 9. Palindrome Number.java

---
+++ 9. Palindrome Number.java
@@ -0,0 +1,16 @@
+Method 1----
+                              class Solution {
+                        public boolean isPalindrome(int x) {
+                            if (x<0||(x%10==0 && x!=0)){
+                                return false;
+                            }
+                             int reversedNum = 0;
+                            while(x>reversedNum)
+                            {
+                                reversedNum=reversedNum*10 +x%10;
+                                x /= 10;
+                            }
+                            return x==reversedNum || x==reversedNum/10;
+
+                        }
+                    }
